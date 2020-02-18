2d3f77939b79bda2a1fd3340fd4edf3eaaae9a73
Zulqadar Idrishi <33245846+Zulqadar@users.noreply.github.com>
2020-02-18 17:58:51 +0530

    Update popup.js

--- My First Extension/popup.js
+++ My First Extension/popup.js
@@ -6,4 +6,42 @@ document.addEventListener('DOMContentLoaded', function() {
         window.open('https://www.google.com/search?q=Zulqadar Idrishi','blank');
       });
     }, false);
+
+    var findButton=document.getElementById('btnFind');
+    findButton.addEventListener('click',function(){
+      chrome.tabs.getSelected(null,function(tab){
+        console.log('Find')
+        onWindowLoad();
+      });
+    },false);
   }, false);
+
+  chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getSource") {
+        //message.innerText = request.source;
+        var htmlString=request.source;
+        var extractedEmails=extractEmailsFromString(htmlString);
+        message.innerText=extractedEmails.join('\n');
+    }
+  });
+
+  function onWindowLoad() {
+
+    var message = document.querySelector('#message');
+  
+    chrome.tabs.executeScript(null, {
+      file: "getPagesSource.js"
+    }, function() {
+      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+      if (chrome.runtime.lastError) {
+        message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+      }
+    });
+  
+  }
+
+  function extractEmailsFromString(text){
+    return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+  }
+  
+  //window.onload = onWindowLoad;
